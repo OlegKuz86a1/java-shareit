67d1d00f9022772bfcfda0dd7468529907dec685
Kuznetsov Oleg <olegkuz86a1@yandex.ru>
2024-10-24 16:07:42 +0300

    fix 4

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,44 +1,35 @@
-DROP TABLE IF EXISTS statuses CASCADE;
-DROP TABLE IF EXISTS booking_status ;
-DROP TABLE IF EXISTS bookings ;
-DROP TABLE IF EXISTS item_requests CASCADE;
-DROP TABLE IF EXISTS comments CASCADE;
-DROP TABLE IF EXISTS items ;
-DROP TABLE IF EXISTS users CASCADE;
-
 
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(256),
-    email VARCHAR(256)
+    name VARCHAR(256)NOT NULL,
+    email VARCHAR(256)NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS item_requests (
     id SERIAL PRIMARY KEY,
-    requestor_id BIGINT,
-    description VARCHAR(256),
-    created TIMESTAMP,
+    requestor_id BIGINT NOT NULL,
+    description VARCHAR(256) NOT NULL,
+    created TIMESTAMP NOT NULL,
     FOREIGN KEY (requestor_id)  REFERENCES users (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS items (
     id SERIAL PRIMARY KEY,
-    owner_id BIGINT,
-    name VARCHAR(256),
-    description VARCHAR(256),
-    available BOOLEAN,
-    request_id BIGINT,
-    comment VARCHAR(256),
+    owner_id BIGINT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    description VARCHAR(256) NOT NULL,
+    available BOOLEAN NOT NULL,
+    request_id BIGINT NOT NULL,
     FOREIGN KEY (owner_id)  REFERENCES users (id) ON DELETE CASCADE,
     FOREIGN KEY (request_id)  REFERENCES item_requests (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS bookings (
     id SERIAL PRIMARY KEY,
-    item_id BIGINT,
-    start_booking TIMESTAMP,
-    end_booking TIMESTAMP,
-    booker_id BIGINT,
+    item_id BIGINT NOT NULL,
+    start_booking TIMESTAMP NOT NULL,
+    end_booking TIMESTAMP NOT NULL,
+    booker_id BIGINT NOT NULL,
     FOREIGN KEY (item_id)  REFERENCES items (id) ON DELETE CASCADE,
     FOREIGN KEY (booker_id)  REFERENCES users (id) ON DELETE CASCADE
 );
